dba5f6d3526c173179efd668f6a2e486f2aa0088
hanriverkitty <danieltls@naver.com>
2024-09-19 01:32:34 +0900

    Refactor: 스타일코드 styles로 리팩토링

--- components/Table.js
+++ components/Table.js
@@ -171,71 +171,22 @@ const data = [
 export const TableChart = () => {
   return (
     <View style={{ flex: 1, width: "100%" }}>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          backgroundColor: "#ffffff",
-          borderRadius: 10,
-          borderColor: "#BFF6C3",
-          borderWidth: 2,
-          elevation: 5,
-          shadowColor: "#dddddd",
-          shadowOpacity: 1,
-          shadowOffset: { width: 0, height: 0 },
-          shadowRadius: 6,
-        }}
-      >
+      <View style={styles.tableFrame}>
         <View style={{ flex: 3 }}>
-          <View
-            style={{
-              height: "10%",
-              justifyContent: "center",
-              borderBottomWidth: 1,
-              borderRightWidth: 1,
-              borderColor: "#dddddd",
-              backgroundColor: "#83DB89",
-              borderTopLeftRadius: 8,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: 600,
-                textAlign: "center",
-                textAlignVertical: "center",
-                color: "white",
-              }}
-            >
-              {" "}
-              재료 번호
-            </Text>
+          <View style={styles.materialColumn}>
+            <Text style={styles.materialColumnText}> 재료 번호</Text>
           </View>
           {materialNo.map((material, index) => (
             <View
               key={index}
               style={[
-                {
-                  height: "9%",
-                  justifyContent: "center",
-                  borderBottomWidth: 1,
-                  borderRightWidth: 1,
-                  borderColor: "#dddddd",
-                },
+                styles.materialNo,
                 index == materialNo.length - 1 && { borderBottomWidth: 0 },
                 index % 2 != 0 && { backgroundColor: "#E8FFE2" },
                 //  회색 #eeeeee
               ]}
             >
-              <Text
-                key={index}
-                style={{
-                  textAlign: "center",
-                  textAlignVertical: "center",
-                  fontSize: 16,
-                  fontWeight: 600,
-                }}
-              >
+              <Text key={index} style={styles.materialNoText}>
                 {material}
               </Text>
             </View>
@@ -245,39 +196,13 @@ export const TableChart = () => {
           <View>
             <ScrollView horizontal={true}>
               <View style={{ flexDirection: "column" }}>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    height: "10%",
-                    backgroundColor: "#83DB89",
-                    borderTopRightRadius: 8,
-                  }}
-                >
+                <View style={styles.headerView}>
                   {headerData.map((header, index) => (
                     <View
                       key={index}
-                      style={[
-                        index == 2
-                          ? {
-                              width: 300,
-                              justifyContent: "center",
-                              borderBottomWidth: 1,
-                              borderColor: "#dddddd",
-                            }
-                          : styles.cell,
-                        { backgroundColor: "#83DB89", borderTopRightRadius: 8 },
-                      ]}
+                      style={index == 2 ? styles.header : styles.cell}
                     >
-                      <Text
-                        key={index}
-                        style={{
-                          textAlign: "center",
-                          textAlignVertical: "center",
-                          fontSize: 18,
-                          fontWeight: 700,
-                          color: "white",
-                        }}
-                      >
+                      <Text key={index} style={styles.headerText}>
                         {header}
                       </Text>
                     </View>
@@ -289,7 +214,13 @@ export const TableChart = () => {
                   {data.map((row, index) => (
                     <View
                       key={index}
-                      style={{ flexDirection: "row", height: "10%" }}
+                      style={[
+                        styles.rowView,
+                        index == data.length - 1 && {
+                          borderBottomWidth: 0,
+                        },
+                        index % 2 != 0 && { backgroundColor: "#E8FFE2" },
+                      ]}
                     >
                       {row.map((rowData, cellIndex) => (
                         <View
@@ -299,26 +230,11 @@ export const TableChart = () => {
                               ? {
                                   width: 300,
                                   justifyContent: "center",
-                                  borderBottomWidth: 1,
-                                  borderColor: "#dddddd",
                                 }
                               : styles.cell,
-                            index == data.length - 1 && {
-                              borderBottomWidth: 0,
-                            },
-                            index % 2 != 0 && { backgroundColor: "#E8FFE2" },
-                            //  회색 #eeeeee
                           ]}
                         >
-                          <Text
-                            style={{
-                              textAlign: "center",
-                              textAlignVertical: "center",
-                              fontSize: 16,
-                            }}
-                          >
-                            {rowData}
-                          </Text>
+                          <Text style={styles.cellText}>{rowData}</Text>
                         </View>
                       ))}
                     </View>
@@ -329,16 +245,7 @@ export const TableChart = () => {
           </View>
         </View>
       </View>
-      <View
-        style={{
-          height: "8%",
-          marginTop: "5%",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexDirection: "row",
-          paddingHorizontal: "25%",
-        }}
-      >
+      <View style={styles.pagingView}>
         <TouchableOpacity>
           <MaterialCommunityIcons
             name="chevron-left"
@@ -349,18 +256,10 @@ export const TableChart = () => {
         {Array(5)
           .fill(0)
           .map((_, index) => (
-            <TouchableOpacity
-              key={index}
-              style={{
-                height: 30,
-                width: 30,
-                borderRadius: 6,
-                justifyContent: "center",
-              }}
-            >
+            <TouchableOpacity key={index} style={styles.pagingBtn}>
               <Text
                 style={[
-                  { textAlign: "center", color: "#262627", fontSize: 16 },
+                  styles.pagingText,
                   index === 2 && { color: "#83DB89", fontWeight: 800 },
                 ]}
               >
@@ -386,7 +285,98 @@ const styles = StyleSheet.create({
   cell: {
     width: 100,
     justifyContent: "center",
+  },
+  tableFrame: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    borderColor: "#BFF6C3",
+    borderWidth: 2,
+    elevation: 5,
+    shadowColor: "#dddddd",
+    shadowOpacity: 1,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 6,
+  },
+  materialColumn: {
+    height: "10%",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#dddddd",
+    backgroundColor: "#83DB89",
+    borderTopLeftRadius: 8,
+  },
+  materialColumnText: {
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "white",
+  },
+  materialNo: {
+    height: "9%",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#dddddd",
+  },
+  materialNoText: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  headerView: {
+    flexDirection: "row",
+    height: "10%",
+    backgroundColor: "#83DB89",
+    borderTopRightRadius: 8,
     borderBottomWidth: 1,
     borderColor: "#dddddd",
+  },
+  header: {
+    width: 300,
+    justifyContent: "center",
+    backgroundColor: "#83DB89",
+    borderTopRightRadius: 8,
+  },
+  headerText: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 18,
+    fontWeight: "700",
+    color: "white",
+  },
+  rowView: {
+    flexDirection: "row",
+    height: "10%",
+    borderBottomWidth: 1,
+    borderColor: "#dddddd",
+  },
+  cellText: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 16,
+  },
+  pagingView: {
+    height: "8%",
+    marginTop: "5%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: "25%",
+  },
+  pagingBtn: {
+    height: 30,
+    width: 30,
+    borderRadius: 6,
+    justifyContent: "center",
+  },
+  pagingText: {
+    textAlign: "center",
+    color: "#262627",
+    fontSize: 16,
   },
 });
