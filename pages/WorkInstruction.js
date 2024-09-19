085cb5988306888a922a805d1a6362afb9f27f1e
hanriverkitty <danieltls@naver.com>
2024-09-19 10:54:44 +0900

    Feat: webView

--- pages/WorkInstruction.js
+++ pages/WorkInstruction.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { WebView } from "react-native-webview";
 
 export const WorkInstruction = () => {
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 2, backgroundColor: "#E8FFE2" }}>
-        <Text>WorkInstruction</Text>
+        <WebView source={{ uri: "https://naver.com" }}></WebView>
       </View>
       <View style={{ flex: 1, backgroundColor: "#ffffff" }}>
         <Text>WorkInstruction</Text>
